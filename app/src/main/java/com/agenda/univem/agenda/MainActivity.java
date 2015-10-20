c7eeed396d5afdf0dacc159cede032fce8b071de
R <sabatine2@gmail.com>
2015-10-19 22:47:53 -0200

    Commmit init

---
+++ app/src/main/java/com/agenda/univem/agenda/MainActivity.java
@@ -0,0 +1,95 @@
+package com.agenda.univem.agenda;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.provider.CalendarContract;
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import java.util.Calendar;
+
+
+public class MainActivity extends ActionBarActivity {
+
+    EditText editTextEmail;
+    EditText editTextPass;
+    Button buttonLogin;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        editTextEmail = (EditText) findViewById(R.id.main_email);
+        editTextPass = (EditText) findViewById(R.id.main_pass);
+        buttonLogin = (Button) findViewById(R.id.main_login);
+
+        buttonLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                login();
+            }
+        });
+    }
+
+    private void login(){
+        String email = editTextEmail.getText().toString();
+        String pass = editTextPass.getText().toString();
+
+        if(email.contentEquals("") && pass.contentEquals("")){
+            Toast.makeText(getApplicationContext(),"Login com sucesso",Toast.LENGTH_LONG).show();
+            startMenuActi();
+        }
+        else
+            Log.i("Login","Login falhou");
+    }
+
+    private void startMenuActi(){
+        Intent calendarIntent = new Intent(Intent.ACTION_INSERT, CalendarContract.Events.CONTENT_URI);
+        Calendar beginTime = Calendar.getInstance();
+        beginTime.set(2012, 0, 19, 7, 30);
+        Calendar endTime = Calendar.getInstance();
+        endTime.set(2012, 0, 19, 10, 30);
+        calendarIntent.putExtra(CalendarContract.EXTRA_EVENT_BEGIN_TIME, beginTime.getTimeInMillis());
+        calendarIntent.putExtra(CalendarContract.EXTRA_EVENT_END_TIME, endTime.getTimeInMillis());
+        calendarIntent.putExtra(CalendarContract.Events.TITLE, "Ninja class");
+        calendarIntent.putExtra(CalendarContract.Events.EVENT_LOCATION, "Secret dojo");
+        startActivity(calendarIntent);
+       // Intent intent = new Intent(getApplicationContext(),MenuActivity.class);
+        //startActivity(intent);
+    }
+
+    protected void onPause() {
+        super.onPause();
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
---
+++ app/src/main/java/com/agenda/univem/agenda/model/User.java
@@ -0,0 +1,9 @@
+package com.agenda.univem.agenda.model;
+
+/**
+ * Created by Laboratorio on 19/10/2015.
+ */
+public class User {
+    public String name;
+    public String email;
+}
